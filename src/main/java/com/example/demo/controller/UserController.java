89b793af632ccf8569b32ac5e3c8ecd0b3b73572
SihemHACHANIpro <sihemhachani724@gmail.com>
2024-10-07 20:44:31 +0200

    first commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,108 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entitie.Role;
+import com.example.demo.entitie.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+	
+@Autowired
+private UserRepository userrespo ;
+@Autowired
+private RoleRepository rolerepos ; 
+/*
+ * Getmapping (affichage)
+ * Postmapping (cretaion)
+ * DeleteMapping(suppression)
+ * Putmappring(modification)
+ */
+
+
+@PostMapping("/ajouter")
+public String ajouter(@RequestBody User u) {
+	this.userrespo.save(u);
+	return "enregistrer avec succées";
+}
+
+
+
+@PostMapping("/ajouteravectest")
+public String ajouteravectest(@RequestBody User u) {
+	
+	User user = this.userrespo.findByEmail(u.getEmail());
+	if(user==null) {
+		this.userrespo.save(u);
+		return "enregistrer avec succées";
+		
+	}
+	else {
+		return "email existe deja";
+	}
+}
+
+
+@GetMapping("/afficher")
+public List<User> afficher(){
+return this.userrespo.findAll();
+}
+
+@GetMapping("/afficherbyid")
+public User afficherbbyid(Long id){
+	User u = this.userrespo.findById(id).get();
+return u;
+}
+
+@DeleteMapping("/supprimer")
+public String supprimer(Long id) {
+	User u = this.userrespo.findById(id).get();
+	this.userrespo.delete(u);
+	return "user supprimer avec succées";
+}
+
+@PutMapping("/modifier")
+public String modifier(Long id) {
+	User u = this.userrespo.findById(id).get();
+	u.setPrenom("formateur");
+	this.userrespo.save(u);
+	return "modification a été avec succées" ; 
+}
+
+
+
+@PostMapping("/ajouteravecrole")
+public String ajouteravecrole(@RequestBody User u ,String profil ) {
+
+	Role r = this.rolerepos.findByProfil(profil);
+	u.setRole(r);
+	this.userrespo.save(u);
+	return "true";
+	
+}
+
+@GetMapping("/afficherbyrole")
+public List<User> afficherbyrole(String profil){
+	Role r = this.rolerepos.findByProfil(profil);
+	return this.userrespo.findByRole(r);
+	
+}
+
+
+
+
+
+}
---
+++ src/main/java/com/example/demo/entitie/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.entitie;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id ;
+	private String nom ;
+	private String prenom ;
+	private String email ;
+	
+	@ManyToOne
+	private Role role ; 
+	
+		
+	
+	
+}
